ab8063fcb8a5e5d2bed5c32584e378d1df7d96aa
Moca9801 <aimc9801@gmail.com>
2025-10-01 13:00:28 -0600

    Auth componete has been completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ThemeColorComponent } from './pages/theme-color/theme-color.component';
 import { TranslationComponent } from './pages/translation/translation.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
 export const routes: Routes = [
     { path: 'themeColor', component: ThemeColorComponent },
-    { path: 'translation', component: TranslationComponent }
+    { path: 'translation', component: TranslationComponent },
+    { path: 'login', component: AuthComponent },
 ];
